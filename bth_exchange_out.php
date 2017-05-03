ea2adc2497217d21937aeb6b90db8c5dafeb00b4
koluong <kennyk94@live.com>
2017-05-03 12:44:00 -0700

    includes csrf token check

--- bth_exchange_out.php
+++ bth_exchange_out.php
@@ -1,8 +1,9 @@
 <?php 
-
+session_start();
 require_once('security/validation_functions.php');
 require_once('security/allowed_params.php');
 require_once('security/xss_sanitize_functions.php');
+require_once('security/csrf_token_functions.php');
 
 $valid_confirm = true;
 $parts = explode('@', $_POST['email']);
@@ -10,8 +11,9 @@ $post_params = allowed_post_params(['name', 'phone', 'email', 'receiverName', 'a
 
 if ($_SERVER ['REQUEST_METHOD'] === 'POST') {
 
+    //if (csrf_token_is_valid()) {
     $name = has_presence($_POST['name']) ? $_POST['name'] : "Invalid Name!";
-    $clean_name = h($name);
+    //$clean_name = h($name);
 
     $phone = has_presence($_POST['phone']) ? (
         has_format_matching($_POST['phone'], '/^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/') ? $_POST['phone'] : "Invalid Phone!"
@@ -36,6 +38,17 @@ if ($_SERVER ['REQUEST_METHOD'] === 'POST') {
     } elseif ($valid_confirm) {
         $confirm_header = 'Success | Send Confirmation';
     }
+
+    // } else {
+    //     $confirm_header = 'INVALID SESSION TOKEN';
+    //     $clean_name = 'INVALID SESSION TOKEN';
+    //     $email = 'INVALID SESSION TOKEN';
+    //     $phone = 'INVALID SESSION TOKEN';
+    //     $recipient = 'INVALID SESSION TOKEN';
+    //     $amount = 'INVALID SESSION TOKEN';
+    // }
+
+    
 }
 
 
@@ -53,13 +66,13 @@ if ($_SERVER ['REQUEST_METHOD'] === 'POST') {
         <div class="container">
             <h1> <?php echo $confirm_header; ?> </h1>
             <hr>
-                <h4>Name: <?php echo $clean_name; ?> </h4>
+                <h4>Name: <?php echo $name; ?> </h4>
                 <h4>Email: <?php echo $email; ?> </h4>
                 <h4>Phone: <?php echo $phone; ?> </h4>
                 <h4>Recipient: <?php echo $recipient; ?> </h4>
                 <h4>Amount: <?php echo $amount; ?> </h4>
-                <a href="index.html" class="btn btn-success" role="button">Send Again</a>
-                <a href="index.html" class="btn btn-danger" role="button">Confirm Send</a>
+                <a href="index" class="btn btn-success" role="button">Send Again</a>
+                <a href="index" class="btn btn-danger" role="button">Confirm Send</a>
                 <a href="#" class="btn btn-primary btn-primary" role="button">Dashboard</a>
             </div>
     </body>

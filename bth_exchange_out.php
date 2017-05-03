ff911a2ce3060e55f7b1006240d9ece02fb6ae86
koluong <kennyk94@live.com>
2017-05-03 11:24:40 -0700

    TODO:: implement csrf token

--- bth_exchange_out.php
+++ bth_exchange_out.php
@@ -2,15 +2,16 @@
 
 require_once('security/validation_functions.php');
 require_once('security/allowed_params.php');
+require_once('security/xss_sanitize_functions.php');
 
 $valid_confirm = true;
 $parts = explode('@', $_POST['email']);
 $post_params = allowed_post_params(['name', 'phone', 'email', 'receiverName', 'amount']);
 
 if ($_SERVER ['REQUEST_METHOD'] === 'POST') {
-    var_dump($post_params);
 
     $name = has_presence($_POST['name']) ? $_POST['name'] : "Invalid Name!";
+    $clean_name = h($name);
 
     $phone = has_presence($_POST['phone']) ? (
         has_format_matching($_POST['phone'], '/^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/') ? $_POST['phone'] : "Invalid Phone!"
@@ -26,12 +27,12 @@ if ($_SERVER ['REQUEST_METHOD'] === 'POST') {
         is_numeric($_POST['amount']) ? $_POST['amount'] : "Invalid Amount!"
     ) : "Invalid Amount!";
 
-    if (strpos($name, 'Invalid') !== false || strpos($phone, 'Invalid' || strpos($email, 'Invalid') || strpos($recipient, 'Invalid')) !== false) {
+    if (strpos($name, 'Invalid') !== false || strpos($phone, 'Invalid') !== false || strpos($email, 'Invalid') !== false || strpos($recipient, 'Invalid') !== false) {
         $valid_confirm = false;
     }
 
     if (!$valid_confirm) {
-        $confirm_header = 'Invalid Respone, Please Try Again';
+        $confirm_header = 'Invalid Response, Please Try Again';
     } elseif ($valid_confirm) {
         $confirm_header = 'Success | Send Confirmation';
     }
@@ -52,7 +53,7 @@ if ($_SERVER ['REQUEST_METHOD'] === 'POST') {
         <div class="container">
             <h1> <?php echo $confirm_header; ?> </h1>
             <hr>
-                <h4>Name: <?php echo $name; ?> </h4>
+                <h4>Name: <?php echo $clean_name; ?> </h4>
                 <h4>Email: <?php echo $email; ?> </h4>
                 <h4>Phone: <?php echo $phone; ?> </h4>
                 <h4>Recipient: <?php echo $recipient; ?> </h4>
@@ -60,6 +61,5 @@ if ($_SERVER ['REQUEST_METHOD'] === 'POST') {
                 <a href="index.html" class="btn btn-danger" role="button">Send Again</a>
                 <a href="#" class="btn btn-primary btn-primary" role="button">Dashboard</a>
             </div>
-        </div>
     </body>
 </html>
